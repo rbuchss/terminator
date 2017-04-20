20740ea45c5f4948c18b1808fc9578d62f4f96f6
rbuchss <rbuchss@gmail.com>
2017-04-20 13:50:42 -0400

    dry out tmux styles

--- tmux/helpers/init.sh
+++ tmux/helpers/init.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # to add scripts use this format #(~/.tmux/helpers/battery-health.sh)
-export TMUX_DIVIDER_RIGHT='#[fg=colour39,bg=colour234]⮂#[fg=colour234,bg=colour39]⮂#[fg=colour10,bg=colour234]'
-export TMUX_DIVIDER_LEFT='#[fg=colour234,bg=colour39]⮀#[fg=colour39,bg=colour234,nobold]⮀'
+export TmuxHostColor="colour${HostColorNum}"
+export TmuxBgColor="colour234"
+export TmuxSessionColor="colour10"
+export TmuxMessageColor="colour16"
+export TmuxMenuColor="colour39"
+
+export TMUX_DIVIDER_RIGHT="#[fg=${TmuxMenuColor},bg=${TmuxBgColor}]⮂#[fg=${TmuxBgColor},bg=${TmuxMenuColor}]⮂#[fg=${TmuxSessionColor},bg=${TmuxBgColor}]"
+export TMUX_DIVIDER_LEFT="#[fg=${TmuxBgColor},bg=${TmuxMenuColor}]⮀#[fg=${TmuxMenuColor},bg=${TmuxBgColor},nobold]⮀"
 export TMUX_PREFIX_STAT='#{?client_prefix,#[reverse]<Prefix>#[noreverse] , }'
-export TMUX_STATUS_RIGHT="#[fg=colour10,bg=colour234]${TMUX_PREFIX_STAT}${TMUX_DIVIDER_RIGHT} %a %b %d %R #[default]"
-export TMUX_STATUS_LEFT="#[fg=colour10,bg=colour234] #S #[fg=colour39,bg=colour234]#h $TMUX_DIVIDER_LEFT "
+export TMUX_STATUS_RIGHT="#[fg=${TmuxSessionColor},bg=${TmuxBgColor}]${TMUX_PREFIX_STAT}${TMUX_DIVIDER_RIGHT} %a %b %d %R #[default]"
+export TMUX_STATUS_LEFT="#[fg=${TmuxSessionColor},bg=${TmuxBgColor}] #S #[fg=${TmuxHostColor},bg=${TmuxBgColor}]#h $TMUX_DIVIDER_LEFT "
 export TMUX_STATUS=' #I #W '
-export TMUX_STATUS_CURRENT='#[fg=colour234,bg=colour39,noreverse,bright,nobold] #I ⮁ #W #[fg=colour39,bg=colour234,nobold]'
+export TMUX_STATUS_CURRENT="#[fg=${TmuxMessageColor},bg=${TmuxMenuColor},noreverse,bright,nobold] #I #W #[fg=${TmuxMenuColor},bg=${TmuxBgColor},nobold]"
