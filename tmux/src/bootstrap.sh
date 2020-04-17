a64b275c354bc3a1df1df1626c468933ffceed5f
rbuchss <rbuchss@gmail.com>
2020-04-17 18:29:51 -0400

    changed tmux color constant naming convention to be consistently all caps

--- tmux/src/bootstrap.sh
+++ tmux/src/bootstrap.sh
@@ -39,19 +39,18 @@ function tmux::bootstrap::session_create() {
 
 function tmux::bootstrap::load_style_environment_vars() {
   # to add scripts use this format #(~/.tmux/helpers/battery-health.sh)
-  export TmuxHostColor="colour${HostColorNum}"
-  export TmuxBgColor="colour234"
-  export TmuxSessionColor="colour10"
-  export TmuxMessageColor="colour16"
-  export TmuxMenuColor="colour39"
-
-  export TMUX_DIVIDER_RIGHT="#[fg=${TmuxMenuColor},bg=${TmuxBgColor}]⮂#[fg=${TmuxBgColor},bg=${TmuxMenuColor}]⮂#[fg=${TmuxSessionColor},bg=${TmuxBgColor}]"
-  export TMUX_DIVIDER_LEFT="#[fg=${TmuxBgColor},bg=${TmuxMenuColor}]⮀#[fg=${TmuxMenuColor},bg=${TmuxBgColor},nobold]⮀"
+  export TMUX_HOST_COLOR="colour${HostColorNum}"
+  export TMUX_BG_COLOR="colour234"
+  export TMUX_SESSION_COLOR="colour10"
+  export TMUX_MESSAGE_COLOR="colour16"
+  export TMUX_MENU_COLOR="colour39"
+  export TMUX_DIVIDER_RIGHT="#[fg=${TMUX_MENU_COLOR},bg=${TMUX_BG_COLOR}]⮂#[fg=${TMUX_BG_COLOR},bg=${TMUX_MENU_COLOR}]⮂#[fg=${TMUX_SESSION_COLOR},bg=${TMUX_BG_COLOR}]"
+  export TMUX_DIVIDER_LEFT="#[fg=${TMUX_BG_COLOR},bg=${TMUX_MENU_COLOR}]⮀#[fg=${TMUX_MENU_COLOR},bg=${TMUX_BG_COLOR},nobold]⮀"
   export TMUX_PREFIX_STAT='#{?client_prefix,#[reverse]<Prefix>#[noreverse] , }'
-  export TMUX_STATUS_RIGHT="#[fg=${TmuxSessionColor},bg=${TmuxBgColor}]${TMUX_PREFIX_STAT}${TMUX_DIVIDER_RIGHT} %a %b %d %R #[default]"
-  export TMUX_STATUS_LEFT="#[fg=${TmuxSessionColor},bg=${TmuxBgColor}] #S #[fg=${TmuxHostColor},bg=${TmuxBgColor}]#h $TMUX_DIVIDER_LEFT "
+  export TMUX_STATUS_RIGHT="#[fg=${TMUX_SESSION_COLOR},bg=${TMUX_BG_COLOR}]${TMUX_PREFIX_STAT}${TMUX_DIVIDER_RIGHT} %a %b %d %R #[default]"
+  export TMUX_STATUS_LEFT="#[fg=${TMUX_SESSION_COLOR},bg=${TMUX_BG_COLOR}] #S #[fg=${TMUX_HOST_COLOR},bg=${TMUX_BG_COLOR}]#h $TMUX_DIVIDER_LEFT "
   export TMUX_STATUS=' #I #W '
-  export TMUX_STATUS_CURRENT="#[fg=${TmuxMessageColor},bg=${TmuxMenuColor},noreverse,bright,nobold] #I #W #[fg=${TmuxMenuColor},bg=${TmuxBgColor},nobold]"
+  export TMUX_STATUS_CURRENT="#[fg=${TMUX_MESSAGE_COLOR},bg=${TMUX_MENU_COLOR},noreverse,bright,nobold] #I #W #[fg=${TMUX_MENU_COLOR},bg=${TMUX_BG_COLOR},nobold]"
 }
 
 function tmux::bootstrap::version_config_path() {
