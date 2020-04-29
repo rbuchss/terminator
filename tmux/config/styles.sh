7c61cac31704693a54c1a7f52f6ff1d21cbf3b57
rbuchss <rbuchss@gmail.com>
2020-04-28 23:39:00 -0400

    wrap long tmux style lines

--- tmux/config/styles.sh
+++ tmux/config/styles.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 
-# to add scripts use this format #(~/.tmux/bin/foobar.sh)
+# NOTE: to add scripts use this format #(~/.tmux/bin/foobar.sh)
 TMUX_HOST_COLOR="colour${HostColorNum}"
 TMUX_BG_COLOR="colour234"
 TMUX_SESSION_COLOR="colour10"
 TMUX_MESSAGE_COLOR="colour16"
 TMUX_MENU_COLOR="colour39"
-TMUX_DIVIDER_RIGHT="#[fg=${TMUX_MENU_COLOR},bg=${TMUX_BG_COLOR}]⮂#[fg=${TMUX_BG_COLOR},bg=${TMUX_MENU_COLOR}]⮂#[fg=${TMUX_SESSION_COLOR},bg=${TMUX_BG_COLOR}]"
-TMUX_DIVIDER_LEFT="#[fg=${TMUX_BG_COLOR},bg=${TMUX_MENU_COLOR}]⮀#[fg=${TMUX_MENU_COLOR},bg=${TMUX_BG_COLOR},nobold]⮀"
+
+TMUX_DIVIDER_RIGHT="\
+#[fg=${TMUX_MENU_COLOR},bg=${TMUX_BG_COLOR}]\
+⮂#[fg=${TMUX_BG_COLOR},bg=${TMUX_MENU_COLOR}]\
+⮂#[fg=${TMUX_SESSION_COLOR},bg=${TMUX_BG_COLOR}]"
+
+TMUX_DIVIDER_LEFT="\
+#[fg=${TMUX_BG_COLOR},bg=${TMUX_MENU_COLOR}]⮀\
+#[fg=${TMUX_MENU_COLOR},bg=${TMUX_BG_COLOR},nobold]⮀"
+
 TMUX_PREFIX_STAT='#{?client_prefix,#[reverse]<Prefix>#[noreverse] , }'
-TMUX_STATUS_RIGHT="#[fg=${TMUX_SESSION_COLOR},bg=${TMUX_BG_COLOR}]${TMUX_PREFIX_STAT}${TMUX_DIVIDER_RIGHT} %a %b %d %R #[default]"
-TMUX_STATUS_LEFT="#[fg=${TMUX_SESSION_COLOR},bg=${TMUX_BG_COLOR}] #S #[fg=${TMUX_HOST_COLOR},bg=${TMUX_BG_COLOR}]#h ${TMUX_DIVIDER_LEFT} "
+
+TMUX_STATUS_RIGHT="\
+#[fg=${TMUX_SESSION_COLOR},bg=${TMUX_BG_COLOR}]\
+${TMUX_PREFIX_STAT}${TMUX_DIVIDER_RIGHT} %a %b %d %R #[default]"
+
+TMUX_STATUS_LEFT="\
+#[fg=${TMUX_SESSION_COLOR},bg=${TMUX_BG_COLOR}] #S \
+#[fg=${TMUX_HOST_COLOR},bg=${TMUX_BG_COLOR}]#h ${TMUX_DIVIDER_LEFT} "
+
 TMUX_STATUS=' #I #W '
-TMUX_STATUS_CURRENT="#[fg=${TMUX_MESSAGE_COLOR},bg=${TMUX_MENU_COLOR},noreverse,bright,nobold] #I #W #[fg=${TMUX_MENU_COLOR},bg=${TMUX_BG_COLOR},nobold]"
+
+TMUX_STATUS_CURRENT="\
+#[fg=${TMUX_MESSAGE_COLOR},bg=${TMUX_MENU_COLOR},noreverse,bright,nobold] #I #W \
+#[fg=${TMUX_MENU_COLOR},bg=${TMUX_BG_COLOR},nobold]"
 
 export TMUX_HOST_COLOR
 export TMUX_BG_COLOR
