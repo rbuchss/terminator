1062b872bc5011e3ca73cf3cc9688516ae8d4e51
rbuchss <rbuchss@gmail.com>
2023-05-18 14:00:17 -0400

    add terraform bootstrap and alias

---
+++ terminator/src/terraform.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# shellcheck source=/dev/null
+source "${BASH_SOURCE[0]%/*}/homebrew.sh"
+
+function terminator::terraform::bootstrap() {
+  if command -v terraform > /dev/null 2>&1; then
+    if terminator::homebrew::package::is_installed terraform; then
+      # shellcheck source=/dev/null
+      complete -C "$(brew --prefix)/bin/terraform" terraform
+      complete -C "$(brew --prefix)/bin/terraform" tf
+
+      alias tf='terraform'
+    fi
+  else
+    terminator::log::warning 'terraform is not installed'
+  fi
+}
