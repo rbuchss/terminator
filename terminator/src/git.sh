60a272f43f8e21a1e9c3cfd92e1af120fe57e332
rbuchss <rbuchss@gmail.com>
2023-07-17 21:16:47 -0400

    remove unused git url_parser

--- terminator/src/git.sh
+++ terminator/src/git.sh
@@ -14,11 +14,6 @@ function terminator::git::bootstrap() {
   fi
 }
 
-function terminator::git::url_parse() {
-  sed -E 's#(git\@|https://)([^/:]+)(:|/)([^/]+)/(.+$)#'\\"$2"'#g' \
-    <<< "$1"
-}
-
 function terminator::git::invoke() {
   if command -v hub > /dev/null 2>&1; then
     command hub "$@"
