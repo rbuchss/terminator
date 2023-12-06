8d6b1369bc6e080e81a7b99894abb445add23100
rbuchss <rbuchss@gmail.com>
2023-12-05 21:09:31 -0500

    move double module logic into number module

--- terminator/src/number.sh
+++ terminator/src/number.sh
@@ -4,6 +4,25 @@ source "${BASH_SOURCE[0]%/*}/__module__.sh"
 
 terminator::__module__::load || return 0
 
+function terminator::number::compare() {
+  if (( $# != 2 )); then
+    >&2 echo "ERROR: ${FUNCNAME[0]}: invalid number of arguments"
+    >&2 echo "usage: ${FUNCNAME[0]} rhs lhs"
+    return 4
+  fi
+
+  awk -v lhs="$1" -v rhs="$2" 'BEGIN {
+    if (lhs == rhs) {
+      exit 0
+    } else if (lhs > rhs) {
+      exit 1
+    } else if (lhs < rhs) {
+      exit 2
+    }
+    exit 3
+  }'
+}
+
 function terminator::number::is_integer() {
   if (( $# < 1 )); then
     >&2 echo 'ERROR: invalid # of args: expected at least 1 argument'
@@ -37,11 +56,13 @@ function terminator::number::is_unsigned_integer() {
 }
 
 function terminator::number::__export__() {
+  export -f terminator::number::compare
   export -f terminator::number::is_integer
   export -f terminator::number::is_unsigned_integer
 }
 
 function terminator::number::__recall__() {
+  export -fn terminator::number::compare
   export -fn terminator::number::is_integer
   export -fn terminator::number::is_unsigned_integer
 }
