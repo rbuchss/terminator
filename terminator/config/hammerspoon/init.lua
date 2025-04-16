9143e5d56bed0edd70f6559c35f14f3139b86488
rbuchss <rbuchss@gmail.com>
2025-04-16 14:59:47 -0400

    enable find first available app in hammerspoon

--- terminator/config/hammerspoon/init.lua
+++ terminator/config/hammerspoon/init.lua
@@ -16,47 +16,108 @@ hs.hotkey.bind({"alt"}, "R", function()
   -- hs.alert.show("Config reloaded")
 end)
 
+-- helper for app launchers
+
+local function findFirstAvailableApp(bundleList)
+  for _, bundle in ipairs(bundleList) do
+    local bundleInfo = hs.application.infoForBundleID(bundle)
+
+    if bundleInfo then
+      return bundle
+    end
+  end
+
+  return nil
+end
+
+local function resolveAppFromEnvOrList(envVarName, bundleList)
+  local envApp = os.getenv(envVarName)
+
+
+  if envApp then
+    return envApp
+  end
+
+  return findFirstAvailableApp(bundleList)
+end
+
 -- app launcher
 
-local hotkey_q_app = os.getenv("TERMINATOR_HAMMERSPOON_HOTKEY_Q_APP") or "ChatGPT"
+local hotkey_q_app = resolveAppFromEnvOrList(
+  "TERMINATOR_HAMMERSPOON_HOTKEY_Q_APP",
+  {
+    "com.openai.chat",
+  }
+)
 
 hs.hotkey.bind({"cmd", "alt"}, "Q", function()
-  hs.application.launchOrFocus(hotkey_q_app)
+  hs.application.launchOrFocusByBundleID(hotkey_q_app)
 end)
 
-local hotkey_w_app = os.getenv("TERMINATOR_HAMMERSPOON_HOTKEY_W_APP") or "Spotify"
+local hotkey_w_app = resolveAppFromEnvOrList(
+  "TERMINATOR_HAMMERSPOON_HOTKEY_W_APP",
+  {
+    "com.spotify.client",
+  }
+)
 
 hs.hotkey.bind({"cmd", "alt"}, "W", function()
-  hs.application.launchOrFocus(hotkey_w_app)
+  hs.application.launchOrFocusByBundleID(hotkey_w_app)
 end)
 
-local hotkey_a_app = os.getenv("TERMINATOR_HAMMERSPOON_HOTKEY_A_APP") or "Arc"
+local hotkey_a_app = resolveAppFromEnvOrList(
+  "TERMINATOR_HAMMERSPOON_HOTKEY_A_APP",
+  {
+    "company.thebrowser.Browser",
+  }
+)
 
 hs.hotkey.bind({"cmd", "alt"}, "A", function()
-  hs.application.launchOrFocus(hotkey_a_app)
+  hs.application.launchOrFocusByBundleID(hotkey_a_app)
 end)
 
-local hotkey_s_app = os.getenv("TERMINATOR_HAMMERSPOON_HOTKEY_S_APP") or "Slack"
+local hotkey_s_app = resolveAppFromEnvOrList(
+  "TERMINATOR_HAMMERSPOON_HOTKEY_S_APP",
+  {
+    "com.tinyspeck.slackmacgap",
+  }
+)
 
 hs.hotkey.bind({"cmd", "alt"}, "S", function()
-  hs.application.launchOrFocus(hotkey_s_app)
+  hs.application.launchOrFocusByBundleID(hotkey_s_app)
 end)
 
-local hotkey_z_app = os.getenv("TERMINATOR_HAMMERSPOON_HOTKEY_Z_APP") or "iTerm"
+local hotkey_z_app = resolveAppFromEnvOrList(
+  "TERMINATOR_HAMMERSPOON_HOTKEY_Z_APP",
+  {
+    "com.googlecode.iterm2",
+    "com.apple.Terminal",
+  }
+)
 
 hs.hotkey.bind({"cmd", "alt"}, "Z", function()
-  hs.application.launchOrFocus(hotkey_z_app)
+  hs.application.launchOrFocusByBundleID(hotkey_z_app)
 end)
 
-local hotkey_x_app = os.getenv("TERMINATOR_HAMMERSPOON_HOTKEY_X_APP") or "PyCharm Professional Edition"
+local hotkey_x_app = resolveAppFromEnvOrList(
+  "TERMINATOR_HAMMERSPOON_HOTKEY_X_APP",
+  {
+    "com.jetbrains.pycharm",
+  }
+)
 
 hs.hotkey.bind({"cmd", "alt"}, "X", function()
-  hs.application.launchOrFocus(hotkey_x_app)
+  hs.application.launchOrFocusByBundleID(hotkey_x_app)
 end)
 
-local hotkey_c_app = os.getenv("TERMINATOR_HAMMERSPOON_HOTKEY_C_APP") or "IntelliJ IDEA Ultimate"
+local hotkey_c_app = resolveAppFromEnvOrList(
+  "TERMINATOR_HAMMERSPOON_HOTKEY_C_APP",
+  {
+    "com.jetbrains.intellij",
+  }
+)
 
 hs.hotkey.bind({"cmd", "alt"}, "C", function()
-  hs.application.launchOrFocus(hotkey_c_app)
+  hs.application.launchOrFocusByBundleID(hotkey_c_app)
 end)
 
