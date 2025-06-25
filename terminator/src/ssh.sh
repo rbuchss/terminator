8c87f7a59a80e2244449f42d022b9427f54f8ea8
Russ Buchanan <rbuchss@gmail.com>
2025-06-25 10:32:53 -0400

    add ssh find and add key commands (#37)

--- terminator/src/ssh.sh
+++ terminator/src/ssh.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 # shellcheck source=/dev/null
 source "${BASH_SOURCE[0]%/*}/__module__.sh"
+source "${BASH_SOURCE[0]%/*}/log.sh"
+source "${BASH_SOURCE[0]%/*}/os.sh"
 
 terminator::__module__::load || return 0
+
+function terminator::ssh::__enable__ {
+  alias ssh-init='terminator::ssh::find_and_add_key'
+}
+
+function terminator::ssh::__disable__ {
+  unalias ssh-init
+}
 
 function terminator::ssh::is_ssh_session {
   if [[ -n "${TERMINATOR_SSH_IS_SSH_SESSION}" ]]; then
@@ -54,16 +64,105 @@ function terminator::ssh::is_ssh_sudo {
   return 1
 }
 
+function terminator::ssh::find_keys {
+  find -L \
+    "${HOME}/.ssh" \
+    -name '*.pub' \
+    -depth 1 \
+    -type f \
+    -o \
+    -type l \
+    | sed 's/\.[^.]*$//'
+}
+
+function terminator::ssh::find_and_add_key {
+  local ssh_key_path
+
+  if ! command -v fzf > /dev/null 2>&1; then
+    terminator::log::error 'Requires fzf which was not found - Exiting'
+    return 1
+  fi
+
+  if ! ssh_key_path="$(terminator::ssh::find_keys | fzf --header='Select ssh key to add')" \
+    || [[ -z "${ssh_key_path}" ]]; then
+      terminator::log::warning 'No ssh keys found - Exiting'
+      return 1
+  fi
+
+  terminator::log::info "Selected ssh key: '${ssh_key_path}'"
+
+  terminator::ssh::add_key "${ssh_key_path}"
+}
+
+function terminator::ssh::add_key {
+  local ssh_key_path="${1:?}"
+
+  if [[ ! -f "${ssh_key_path}" ]]; then
+    terminator::log::warning "Cannot find ssh private key: '${ssh_key_path}' - Exiting"
+    return 1
+  fi
+
+  if [[ ! -f "${ssh_key_path}.pub" ]]; then
+    terminator::log::warning "Cannot find ssh public key: '${ssh_key_path}.pub' - Exiting"
+    return 1
+  fi
+
+  # Check if public key already exists in ssh authentication agent - if so just noop
+  if command ssh-add -L | command grep -q -f "${ssh_key_path}.pub"; then
+    terminator::log::info "ssh-identity '${ssh_key_path}' found in authentication agent - Noop"
+    return
+  fi
+
+  terminator::log::info "ssh-identity '${ssh_key_path}' NOT found in authentication agent - Adding"
+
+  terminator::os::switch \
+    --darwin terminator::ssh::add_key::os::darwin \
+    --linux terminator::ssh::add_key::os::linux \
+    --windows terminator::ssh::add_key::os::windows \
+    --unsupported terminator::ssh::add_key::os::unsupported
+}
+
+function terminator::ssh::add_key::os::darwin {
+  command ssh-add --apple-use-keychain "${1:?}"
+}
+
+function terminator::ssh::add_key::os::linux {
+  command ssh-add "${1:?}"
+}
+
+function terminator::ssh::add_key::os::windows {
+  command ssh-add "${1:?}"
+}
+
+function terminator::ssh::add_key::os::unsupported {
+  terminator::log::error "OS '${OSTYPE}' not supported"
+  return 1
+}
+
 function terminator::ssh::__export__ {
   export -f terminator::ssh::is_ssh_session
   export -f terminator::ssh::ppinfo
   export -f terminator::ssh::is_ssh_sudo
+  export -f terminator::ssh::find_keys
+  export -f terminator::ssh::find_and_add_key
+  export -f terminator::ssh::add_key
+  export -f terminator::ssh::add_key::os::darwin
+  export -f terminator::ssh::add_key::os::linux
+  export -f terminator::ssh::add_key::os::windows
+  export -f terminator::ssh::add_key::os::unsupported
 }
 
 function terminator::ssh::__recall__ {
   export -fn terminator::ssh::is_ssh_session
   export -fn terminator::ssh::ppinfo
   export -fn terminator::ssh::is_ssh_sudo
+  export -fn terminator::ssh::find_keys
+  export -fn terminator::ssh::find_and_add_key
+  export -fn terminator::ssh::add_key
+  export -fn terminator::ssh::add_key::os::darwin
+  export -fn terminator::ssh::add_key::os::linux
+  export -fn terminator::ssh::add_key::os::windows
+  export -fn terminator::ssh::add_key::os::unsupported
 }
 
 terminator::__module__::export
