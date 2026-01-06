485ae62599bebd6c5d5100c7dc18a9f54f76c4a4
rbuchss <rbuchss@gmail.com>
2026-01-05 20:16:43 -0500

    add screenshots dir config

--- terminator/src/os/darwin.sh
+++ terminator/src/os/darwin.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 # shellcheck source=/dev/null
 source "${TERMINATOR_MODULE_SRC_DIR:-${BASH_SOURCE[0]%/*/*}}/__module__.sh"
+source "${TERMINATOR_MODULE_SRC_DIR:-${BASH_SOURCE[0]%/*/*}}/logger.sh"
 source "${TERMINATOR_MODULE_SRC_DIR:-${BASH_SOURCE[0]%/*/*}}/path.sh"
 source "${TERMINATOR_MODULE_SRC_DIR:-${BASH_SOURCE[0]%/*/*}}/source.sh"
 
 terminator::__module__::load || return 0
 
 function terminator::os::darwin::__enable__ {
+  # Set default screenshots directory
+  export TERMINATOR_SCREENSHOTS_DIR="${TERMINATOR_SCREENSHOTS_DIR:-${HOME}/Desktop/Screenshots}"
+
   alias show-files='terminator::os::darwin::finder::show_hidden_files'
   alias hide-files='terminator::os::darwin::finder::hide_hidden_files'
+  alias set-screenshot-dir='terminator::os::darwin::screencapture::set_location'
 }
 
 function terminator::os::darwin::finder::show_hidden_files {
@@ -21,20 +26,37 @@ function terminator::os::darwin::finder::hide_hidden_files {
 
 function terminator::os::darwin::finder::set_show_all_files {
   local value="${1:-NO}"
-  defaults write com.apple.finder AppleShowAllFiles "${value}" \
-    && killall Finder /System/Library/CoreServices/Finder.app
+  defaults write com.apple.finder AppleShowAllFiles "${value}" &&
+    killall Finder /System/Library/CoreServices/Finder.app
+}
+
+function terminator::os::darwin::screencapture::set_location {
+  local location="${1:-${TERMINATOR_SCREENSHOTS_DIR}}"
+
+  # Create directory if it doesn't exist
+  if [[ ! -d "${location}" ]]; then
+    mkdir -p "${location}"
+  fi
+
+  # Set the screenshot location
+  defaults write com.apple.screencapture location "${location}" &&
+    killall SystemUIServer
+
+  terminator::logger::info "Screenshot location set to: ${location}"
 }
 
 function terminator::os::darwin::__export__ {
   export -f terminator::os::darwin::finder::show_hidden_files
   export -f terminator::os::darwin::finder::hide_hidden_files
   export -f terminator::os::darwin::finder::set_show_all_files
+  export -f terminator::os::darwin::screencapture::set_location
 }
 
 function terminator::os::darwin::__recall__ {
   export -fn terminator::os::darwin::finder::show_hidden_files
   export -fn terminator::os::darwin::finder::hide_hidden_files
   export -fn terminator::os::darwin::finder::set_show_all_files
+  export -fn terminator::os::darwin::screencapture::set_location
 }
 
 terminator::__module__::export
