8c477a80d72dff2b3350e7aefb7e067dd1e42695
Russ Buchanan <rbuchss@gmail.com>
2026-02-22 16:03:49 -0500

    deprecate git module in favor of git friends impl (#101)

--- terminator/config/__modules__.sh
+++ terminator/config/__modules__.sh
@@ -13,7 +13,6 @@ terminator::source \
   "${HOME}/.terminator/src/file.sh" \
   "${HOME}/.terminator/src/gcloud.sh" \
   "${HOME}/.terminator/src/ghostty.sh" \
-  "${HOME}/.terminator/src/git.sh" \
   "${HOME}/.terminator/src/go.sh" \
   "${HOME}/.terminator/src/grc.sh" \
   "${HOME}/.terminator/src/grep.sh" \
@@ -56,7 +55,6 @@ __modules__=(
   terminator::file
   terminator::gcloud
   terminator::ghostty
-  terminator::git
   terminator::go
   terminator::grep
   terminator::history
