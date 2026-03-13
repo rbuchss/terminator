9e9fd48a45b8eee3394ff8ad8b0276cfb3d407ad
rbuchss <rbuchss@gmail.com>
2026-03-13 13:43:41 -0400

    fix go path for linux

--- terminator/src/go.sh
+++ terminator/src/go.sh
@@ -7,6 +7,14 @@ source "${TERMINATOR_MODULE_SRC_DIR:-${BASH_SOURCE[0]%/*}}/path.sh"
 terminator::__module__::load || return 0
 
 function terminator::go::__enable__ {
+  # Well-known Go install locations not on PATH by default
+  local __go_path__
+  for __go_path__ in '/usr/local/go/bin' "${HOME}/go/bin"; do
+    if [[ -d "${__go_path__}" ]]; then
+      terminator::path::prepend "${__go_path__}"
+    fi
+  done
+
   terminator::command::exists -v go || return
 
   GOPATH="$(go env GOPATH)"
@@ -17,10 +25,12 @@ function terminator::go::__enable__ {
 }
 
 function terminator::go::__disable__ {
+  terminator::path::remove "${GOBIN}"
+  terminator::path::remove '/usr/local/go/bin'
+  terminator::path::remove "${HOME}/go/bin"
+
   unset GOPATH
   unset GOBIN
-
-  terminator::path::remove "${GOBIN}"
 }
 
 function terminator::go::__export__ {
